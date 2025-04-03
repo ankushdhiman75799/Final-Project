cc05232e3a8f1dfa8edf3d1ca7318f7eb88b9e3e
Ankush Kumar <ankushdhiman75799@gmail.com>
2025-04-03 14:56:23 -0400

    updatein home.php

--- view/home.php
+++ view/home.php
@@ -2,5 +2,5 @@
     <h1> Welcome <?php echo $username ?></h1>
     <h2> Email :<?php echo $email ?></h2>
     <h2> Mobile :<?php echo $mobile ?></h2>
-
+    <h2> Address :<?php echo $address ?></h2>
 </main>
